b3e49374134c86217ac8a325a5186331e69df31e
Suwijak123 <1suwijakt@gmail.com>
2026-07-20 17:53:14 +0700

    q3

---
+++ q3_async.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+Future<Map<String, dynamic>> fetchOrder(int orderId) async {
+  await Future.delayed(Duration(seconds: 2));
+
+  if (orderId <= 0) {
+    throw Exception('รหัสคำสั่งซื้อไม่ถูกต้อง');
+  }
+
+  return {
+    'id': orderId,
+    'menu': 'กาแฟดำ',
+    'total': 35.0,
+  };
+}
+
+Stream<String> trackOrder() async* {
+
+  await Future.delayed(Duration(seconds: 1));
+  yield 'รับออร์เดอร์แล้ว';
+
+
+  await Future.delayed(Duration(seconds: 1));
+  yield 'กำลังจัดทำ';
+
+
+  await Future.delayed(Duration(seconds: 1));
+  yield 'จัดส่งเรียบร้อย';
+}
+
+void main() async {
+  print('เริ่มโหลดข้อมูล...');
+
+  try {
+    var order = await fetchOrder(0);
+    print('ได้ข้อมูลออร์เดอร์: $order');
+  } catch (error) {
+    print('เกิดข้อผิดพลาด: $error');
+  } finally {
+   
+    print('จบรายการ');
+  }
+
+  print('---');
+
+
+  try {
+    var order = await fetchOrder(0);
+    print('ได้ข้อมูลออร์เดอร์: $order');
+  } catch (error) {
+    
+    print('เกิดข้อผิดพลาด: $error');
+  } finally {
+    
+    print('จบรายการ');
+  }
+
+  print('---');
+
+  await for (var status in trackOrder()) {
+    print('สถานะ: $status');
+  }
+  
+  print('ติดตามสถานะเสร็จสิ้น');
+}
